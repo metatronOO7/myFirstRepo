ae47f8b3efb4f544275d8ad9e853a572f10b9db1
Sean Doyle <sdoylelambda@gmail.com>
2018-10-02 12:56:19 -0400

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+//Write a function that takes in an `Int`, and returns two `Int`s in a tuple.
+//Using the provided array of `Int`s or your own, the function should find two numbers from the array
+//that added together will equal the `Int` that you passed in, and return them.
+//If no two numbers sum together to produce the number passed into the function, it should return nil.
+
+// BEFORE YOU START
+//
+// Think of some cases where this function could fail!
+// I can think of at least 2 off the top of my head
+//
+// 1.
+// 2.
+//
+// Come up with ways to address those cases as part of
+// developing your solution!
+
+
+let numbers = [2, 1, 4, 5, 7, 8]
+
+print("hello world")
+
